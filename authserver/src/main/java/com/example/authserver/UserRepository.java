9a7b01347afb6630eb74c7ec1c327c8caf534f47
Deborah Barnard <starfallprojects@gmail.com>
2018-10-19 14:25:32 +0100

    authserver

---
+++ authserver/src/main/java/com/example/authserver/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.authserver;
+import org.springframework.data.ldap.repository.LdapRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends LdapRepository<User> {
+    User findByUsernameAndPassword(String username, String password);
+}
