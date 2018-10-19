87132da0fd6bfc59f692c60afdfddea9c8bf83de
Ethiel ADIASSA <ethiel97@gmail.com>
2018-10-19 16:56:35 +0000

    Update Todo.vue

--- realtime_todo/resources/js/components/Todo.vue
+++ realtime_todo/resources/js/components/Todo.vue
@@ -1,27 +1,22 @@
 
 <template>
- <li class="todo" :class="{ completed: completed }">
-     <div class="view">
-         <input type="checkbox" v-model="completed" class="toggle">
-         <label>{{todo.title}}</label>
-         <button @click="removeTodo(todo)" class="destroy"></button>
-     </div>
- </li>
+    <li class="todo" :class="{ completed: todo.completed }">
+        <div class="view">
+            <input type="checkbox" v-model="todo.completed" class="toggle">
+            <label>{{todo.title}}</label>
+            <button @click="removeTodo(todo)" class="destroy"></button>
+        </div>
+    </li>
 </template>
 <script>
-export default {
-  name: "Todo",
-  props: ["todo"],
-  computed: {
-    completed() {
-        return this.todo.completed !== 0;
-    }
-},
-  methods: {
-    removeTodo(todo) {
-      this.$store.state.toRemove = todo;
-      this.$store.dispatch("DELETE_TODO", todo);
-    }
-  }
-};
+    export default {
+        name: "Todo",
+        props: ["todo"],
+        methods: {
+            removeTodo(todo) {
+                this.$store.commit("CACHE_REMOVED", todo)
+                this.$store.dispatch("DELETE_TODO", todo)
+            }
+        },
+    };
 </script>
