b1b83d83bb94d9138d5b21ef26663ee227b85bc5
Ethiel ADIASSA <ethiel97@gmail.com>
2018-10-19 16:59:08 +0000

    Update TodoApp.vue

--- realtime_todo/resources/js/components/TodoApp.vue
+++ realtime_todo/resources/js/components/TodoApp.vue
@@ -1,31 +1,33 @@
-
 <template>
-     <section class="todoapp">
-            <header class="header">
-           <h1>todos</h1>
-            </header>
-            <new-todo></new-todo>
-            <todo-list></todo-list>
-     </section>
- </template>
- <script>
-import newTodo from "../components/NewTodo.vue";
-import todoList from "../components/TodoList.vue";
-import { mapGetters } from "vuex";
-export default {
-  name: "TodoApp",
-  mounted() {
-    window.Echo.channel("newTask").listen(".task-created", e => {
-      this.$store.commit("ADD_TODO", e.task);
-      this.newTodo.title = "";
-    });
-    window.Echo.channel("taskRemoved").listen(".task-removed", e => {
+    <section class="todoapp">
+        <header class="header">
+            <h1>todos</h1>
+        </header>
+        <new-todo></new-todo>
+        <todo-list></todo-list>
+    </section>
+</template>
+<script>
+    import newTodo from "../components/NewTodo.vue";
+    import todoList from "../components/TodoList.vue";
+    import {mapGetters} from "vuex";
 
-        this.$store.commit("DELETE_TODO", this.toRemove);
-    });
-  },
-  computed: {
-    ...mapGetters(["newTodo", "toRemove"])
-  }
-};
+    export default {
+        name: "TodoApp",
+        components: {
+            todoList
+        },
+        mounted() {
+            window.Echo.channel("newTask").listen(".task-created", e => {
+                this.$store.commit("ADD_TODO", e.task);
+                this.newTodo.title = "";
+            });
+            window.Echo.channel("taskRemoved").listen(".task-removed", e => {
+                this.$store.commit("DELETE_TODO", this.toRemove);
+            });
+        },
+        computed: {
+            ...mapGetters(["newTodo", "toRemove"])
+        }
+    };
 </script>
